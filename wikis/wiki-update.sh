ef90fde318bffd6f9b07b274b0e4eb19c2088c9c
Connor Quick <cquick197@gmail.com>
2018-01-12 15:17:10 -0500

    Adds MacOS support
    Checks if the system is Linux or MacOS and uses GNU find or BSD find
    respectively

--- wikis/wiki-update.sh
+++ wikis/wiki-update.sh
@@ -3,8 +3,19 @@
 cd $(dirname $0)
 
 IFS=$'\n'
-files=$(find "$(pwd -P)" -type f -name "*.md" -printf '"%p"\n' | sort)
 TARGET=../webs/wikis.js
+
+# os detection for linux vs macos (bsd vs gnu find)
+find_base="find \"$(pwd -P)\" -type f -name \"*md\" -print"
+find_linux="f '\"%p\"\n'"
+find_macos="0 | xargs -0 stat -f '%N'"
+case $(uname -s) in
+    Linux*)     find_cmd="$find_base$find_linux | sort";;
+    Darwin*)    find_cmd="$find_base$find_macos | sort";;
+    *)          echo "os not supported...(yet)";exit -1;;
+esac
+
+files=$(eval $find_cmd)
 
 echo "var wikiLists = [" > $TARGET
 for f in $files
